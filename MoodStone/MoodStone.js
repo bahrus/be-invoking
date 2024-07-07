d3ccbf1591fa1567f5d46c68d1c2d6a72416da2f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-07 11:48:28 -0400

    Update README.md

--- MoodStone/MoodStone.js
+++ MoodStone/MoodStone.js
@@ -9,14 +9,16 @@ export class MoodStone extends HTMLElement {
     }
     connectedCallback() {
         this.shadowRoot.innerHTML = String.raw `
+        <soul-searcher -engage-in-second-guessing></soul-searcher>
         <div>
             <h3>Example 1a</h3>
-            <input disabled be-invoking='howAmIFeelingAboutToday'>
+            <input disabled be-invoking=howAmIFeelingAboutToday>
             <h3>Example 1b</h3>
             <input disabled 🕹️='howAmIFeelingAboutToday on change'>
             <h3>Example 1c</h3>
-            <soul-searcher -engage-in-second-guessing></soul-searcher>
-            <input disabled 🕹️='-engage-in-second-guessing'>
+            <input disabled 🕹️=-engage-in-second-guessing>
+            <h3>Example 1d</h3>
+            <input disabled 🕹️=~soulSearcher:engageInSecondGuessing>
         </div>
         <be-hive></be-hive>
         `;
